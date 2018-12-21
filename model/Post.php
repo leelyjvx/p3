0d6d8fccf2648c10ff86dcdf9cd447294489d88b
leelyjvx <lena.dupontbroutin@gmail.com>
2018-12-21 16:14:11 +0100

    Added setters and getters on Post class

---
+++ model/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+class Post {
+
+	private $_id;
+	private $_title;
+	private $_content;
+	private $_postdate;
+
+	public function getID() {
+		return $this->_id;
+	}
+
+	public function getTitle() {
+		return $this->_title;
+	}
+
+	public function getContent() {
+		return $this->_content;
+	}
+
+	public function getPostDate() {
+		return $this->_postdate;
+	}
+
+	public function setID($id) {
+		$this->_id = $id;
+	}
+
+	public function setTitle($title) {
+		$this->_title = $title;
+	}
+
+	public function setContent($content) {
+		$this->_content = $content;
+	}
+
+	public function setPostDate($postdate) {
+		$this->_postdate = $postdate;
+	}
+
+}
