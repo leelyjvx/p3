177a2113379220a23b6b63a5792dec0f11745afd
leelyjvx <lena.dupontbroutin@gmail.com>
2018-12-22 00:58:43 +0100

    Trying again ...

--- view/backend/listposts.php
+++ view/backend/listposts.php
@@ -11,8 +11,8 @@
 	<p><?= $post->getContent() ?></p>
 
 	<p>
-		<form action="index.php?action=postid" method="get">
-			<input type="submit" name="postid" value="Lire l'article">
+		<form action="index.php?postid=<?= $post->getID() ?>" method="get">
+			<input type="submit" name="postid" value="Lire l'article <?= $post->getID() ?>">
 		</form>
 		<form action="index.php?action=deletepost" method="post">
 			<input type="submit" name="delete_post" value="Supprimer">
