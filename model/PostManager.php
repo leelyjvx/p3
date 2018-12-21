35e4f0bb827ae19c4ba69d0ab8827d9f78115e0c
leelyjvx <lena.dupontbroutin@gmail.com>
2018-12-21 16:21:03 +0100

    Updated class PostManager with CRUD functions

--- model/PostManager.php
+++ model/PostManager.php
@@ -10,4 +10,49 @@ class PostManager extends Manager {
 		$this->_db = $this->dbConnect();
 	}
 
+	public function addPost($post) {
+
+		$req = $this->_db->prepare('INSERT INTO posts(id, title, content, postdate) VALUES (?, ?, ?, NOW())');
+		$req->execute(array($post->getID(), $post->getTitle(), $post->getContent()));
+		return $this->_db->lastInsertId();
+	}
+
+	public function read() {
+
+		$posts = [];
+		$req = $this->_db->query('SELECT title, content FROM posts');
+		while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+			$posts[] = new Post($data['title'], $data['content']);
+		}
+		return $posts;
+	}
+
+	public function readID($id) {
+
+		$req = $this->_db->prepare('SELECT id, title, content FROM posts WHERE id = ?');
+		$req->execute(array($id));
+		$data = $req->fetch(PDO::FETCH_ASSOC);
+		$post = new Post($data['title'], $data['content']);
+		$post->setID($data['id']);
+		return $post;
+
+	}
+
+
+	public function delete($id) {
+
+		$req = $this->_db->prepare('DELETE FROM posts WHERE id = :id');
+		$req->execute([':id' => $id]);
+		return $req;
+
+	}
+
+	public function update($post) {
+
+		$req = $this->_db->prepare('UPDATE posts SET title = :title, content = :content WHERE id = :id');
+		$req->execute([':title' => $post->getTitle(), ':content' => $post->getContent(), ':id' => $post->getID()]);
+		var_dump($req);
+		return $req;
+	}
 }
+
